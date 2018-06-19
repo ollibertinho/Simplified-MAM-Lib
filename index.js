2df53e7934d11e29590e5af6f2c25a357c78fe51
ollibertinho <github@onlinebizz.de>
2018-06-19 14:24:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-require("./simplified.mam.lib.js")
+var SimpleMAM = require("./simplified.mam.lib.js");
+var IOTA = require('iota.lib.js');
 
+var seed = 'YOUR 81 CHARS LONG SEED';
+var iota = new IOTA({ provider: 'https://field.carriota.com:443' })
+var mam = new SimpleMAM(iota, seed, true);
+mam.publishMessage('tangle-army rocks!');
