82559d0536b1ed7cf583e84affc7a623bf5953eb
ollibertinho <github@onlinebizz.de>
2018-06-20 09:22:01 +0200

    updated examples

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var SimpleMAM = require("./simplified.mam.lib.js");
 var IOTA = require("iota.lib.js");
 
-/*
-var root = "9HDASXULRQSZPGIPXXKFSQZCIVMA9XPLDBCWSBAQHUZDDNSPDANJZANOKIXLFUZIXGIDOMNHZBXUOGOSO";
-//static function
-SimpleMAM.fetchMessages(root, function(msg) {
+//Exmaple fetching
+var root =
+  "9HDASXULRQSZPGIPXXKFSQZCIVMA9XPLDBCWSBAQHUZDDNSPDANJZANOKIXLFUZIXGIDOMNHZBXUOGOSO";
+var fetchData = new SimpleMAM.MAMFetchData(root);
+SimpleMAM.MAMLib.fetchMessages(fetchData, function(msg) {
   console.log("Message fetched... ", msg);
-})
-  .then(() => {})
-  .catch(err => {
-    console.log("ERROR:", err.stack);
-  });
-*/
+}).catch(err => {
+  console.log("ERROR:", err);
+});
 
-var seed = "9TANGLEARMYROCKS9999999TANGLEARMY99999999TANGLEARMYROCKS9999999999999999999999999";
+/*
+//Exmpmle publishing
 var iota = new IOTA({ provider: "https://field.carriota.com:443" });
-var mam = new SimpleMAM(iota, seed, true);
+var seed =
+  "9TANGLEARMYROCKS9999999TANGLEARMY99999999TANGLEARMYROCKS9999999999999999999999999";
+var mam = new SimpleMAM.MAMLib(iota, seed, true);
+
 mam.publishMessage("tangle-army rocks!", function(err, data) {
   if (err) {
     console.log(err);
@@ -23,3 +25,4 @@ mam.publishMessage("tangle-army rocks!", function(err, data) {
     console.log("Message published.", data);
   }
 });
+*/
